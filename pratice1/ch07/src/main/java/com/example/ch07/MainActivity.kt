163852bc0ec813b9f3187d55fda2537bb2d31d55
yxxyn20 <dbtndus0527@gmail.com>
2022-04-10 15:11:33 +0900

    ch07 GridLayout 배치

--- pratice1/ch07/src/main/java/com/example/ch07/MainActivity.kt
+++ pratice1/ch07/src/main/java/com/example/ch07/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_linear)
-        setContentView(R.layout.activity_relative)
+        //setContentView(R.layout.activity_relative)
+        setContentView(R.layout.activity_grid)
     }
 }
