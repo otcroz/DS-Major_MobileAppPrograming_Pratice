c053fd15548349d363d414a8135bcff540935925
yxxyn20 <dbtndus0527@gmail.com>
2022-04-10 12:34:30 +0900

    ch06 뷰 클래스: 레이아웃, findViewById, 뷰의 기본 속성, visibility

--- pratice1/app/src/main/java/com/example/pratice/MainActivity.kt
+++ pratice1/app/src/main/java/com/example/pratice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.pratice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // activity_main 화면 출력
+
+        //val btn1 : Button = findViewById(R.id.button1); // 버튼 객체를 불러옴
+        val btn2 = findViewById<Button>(R.id.button1) // 제네릭 타입을 사용
+
+        val invisible = findViewById<Button>(R.id.visible_btn)
+        invisible.setOnClickListener {
+            invisible.visibility = View.VISIBLE
+        }
+
     }
 }
