e70496fde6bb1748f45ca80695421de08ef2f62c
yxxyn20 <dbtndus0527@gmail.com>
2022-04-10 12:56:38 +0900

    ch06 뷰 바인딩: 뷰 바인딩 적용하기 1. build.gradle에서 viewBinding { enabled = true } 설정

--- pratice1/app/src/main/java/com/example/pratice/MainActivity.kt
+++ pratice1/app/src/main/java/com/example/pratice/MainActivity.kt
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.example.pratice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // activity_main 화면 출력
+       // setContentView(R.layout.activity_main) // activity_main 화면 출력
 
         //val btn1 : Button = findViewById(R.id.button1); // 버튼 객체를 불러옴
         val btn2 = findViewById<Button>(R.id.button1) // 제네릭 타입을 사용
 
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.visibleBtn.setOnClickListener {
+            binding.testButton.visibility = View.VISIBLE
+        }
+
+        binding.invisibleBtn.setOnClickListener {
+            binding.testButton.visibility = View.INVISIBLE
+        }
 
     }
 }
