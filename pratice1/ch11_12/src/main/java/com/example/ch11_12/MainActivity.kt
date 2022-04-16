c492ddabb4e1d108c64e805cd574c432b95210d7
yxxyn20 <dbtndus0527@gmail.com>
2022-04-16 13:08:54 +0900

    ch11: 액션바에 메뉴 구성하기 1) 하드코딩으로 작성 2) res/menu에 xml 파일 생성하여 menuInflater에 연결하기

--- pratice1/ch11_12/src/main/java/com/example/ch11_12/MainActivity.kt
+++ pratice1/ch11_12/src/main/java/com/example/ch11_12/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.ch11_12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
+
+    // 액션바에서 메뉴 생성
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //1. 하드코딩으로 작성
+        //val menuItem1 : MenuItem? = menu?.add(0,0,0, "메뉴1")
+        //val menuItem2 : MenuItem? = menu?.add(0,1,0, "메뉴2")
+
+        //2. menu xml 파일 만들기: menuInflater
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+
 }
