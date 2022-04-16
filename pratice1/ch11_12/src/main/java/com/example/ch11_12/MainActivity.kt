6773688ce7e5b03cf5d61fc6ec6a908c3b469a83
yxxyn20 <dbtndus0527@gmail.com>
2022-04-16 13:32:14 +0900

    ch11: 액션바에 액션 뷰 적용하기 1) SearchView: 검색 뷰 2) 이벤트 핸들러 연결: OnQueryTextListener

--- pratice1/ch11_12/src/main/java/com/example/ch11_12/MainActivity.kt
+++ pratice1/ch11_12/src/main/java/com/example/ch11_12/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.ch11_12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,14 +14,33 @@ class MainActivity : AppCompatActivity() {
         
     }
 
-    // 액션바에서 메뉴 생성
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        //1. 하드코딩으로 작성
-        //val menuItem1 : MenuItem? = menu?.add(0,0,0, "메뉴1")
-        //val menuItem2 : MenuItem? = menu?.add(0,1,0, "메뉴2")
 
-        //2. menu xml 파일 만들기: menuInflater
-        menuInflater.inflate(R.menu.menu_main, menu)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // 액션바에서 메뉴 생성
+            //1) 하드코딩으로 작성
+            //val menuItem1 : MenuItem? = menu?.add(0,0,0, "메뉴1")
+            //val menuItem2 : MenuItem? = menu?.add(0,1,0, "메뉴2")
+
+            //2) menu xml 파일 만들기: menuInflater
+            menuInflater.inflate(R.menu.menu_main, menu)
+
+        //2. 액션 뷰: 검색 메뉴에 대한 코드
+        val menuSearch = menu?.findItem(R.id.menu_search) // 메뉴 바인딩
+        val searchView = menuSearch?.actionView as SearchView // 액션 뷰 가져오기
+ 
+        searchView.setOnQueryTextListener (object: SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // 검색어 변경 이벤트
+                Log.d("test_app", "검색어 변경~!")
+
+                return true
+            }
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 키보드 검색 버튼을 클릭한 순간의 이벤트
+                Log.d("test_app", "버튼 클릭")
+                return true
+            }
+        } ) 
 
         return super.onCreateOptionsMenu(menu)
 
