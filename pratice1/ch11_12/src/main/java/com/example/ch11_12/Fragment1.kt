8335ea50413f8735002ecf48ec3982d30a407093
yxxyn20 <dbtndus0527@gmail.com>
2022-04-16 15:25:45 +0900

    ch11: recyclerView의 ItemDecoration 적용하기: 목록 화면 구성 1) RecyclerView.ItemDecoration() 클래스 만들기 2) 함수 오버라이딩: onDraw, onDrawOver, getItemOffsets
    3) onCreateView() 에서 addItemDecoration(MyDecoration(activity as Context)) 적용하기
    
    ** 근데 이거 처음에 실행하면 리사이클러 뷰가 2개로 나뉘어 뜨는 오류가 발생한다 왜지?

--- pratice1/ch11_12/src/main/java/com/example/ch11_12/Fragment1.kt
+++ pratice1/ch11_12/src/main/java/com/example/ch11_12/Fragment1.kt
@@ -1,11 +1,18 @@
 package com.example.ch11_12
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ch11_12.databinding.Fragment1Binding
@@ -39,6 +46,42 @@ class MyAdapter(val datas:MutableList<String>) : RecyclerView.Adapter<RecyclerVi
     }
 }
 
+class MyDecoration(val context: Context) : RecyclerView.ItemDecoration(){
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDraw(c, parent, state)
+    }
+
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(c, parent, state)
+        // 이미지 정 중앙에 배치하기: 스마트폰의 크기 /2 - 이미지 크기 /2
+        // 1. 메인 액티비티의 크기 구하기
+        val width = parent.width
+        val height = parent.height
+
+        // 2. 이미지 액티비티의 크기 구하기
+        val dr: Drawable? = ResourcesCompat.getDrawable(context.resources, R.drawable.kbo, null)
+        val d_width = dr?.intrinsicWidth
+        val d_height = dr?.intrinsicHeight
+
+        val left = width/2 - d_width?.div(2) as Int // 계산 수 Int로
+        val top = height/2 - d_height?.div(2) as Int
+
+        c.drawBitmap(BitmapFactory.decodeResource(context.resources, R.drawable.kbo),left.toFloat(),top.toFloat(),null ) // 이미지, 위치
+    }
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.set(10, 10, 10, 0)
+        view.setBackgroundColor(Color.parseColor("#49c1ff")) // 사용자 지정 색상 설정
+        ViewCompat.setElevation(view, 20.0f) // 3차원적으로 그림을 그려줌(살짝 올라가있는 효과)
+    }
+}
+
 class Fragment1 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -65,9 +108,11 @@ class Fragment1 : Fragment() {
         //리사이클러 뷰 적용하기
         val binding = Fragment1Binding.inflate(inflater, container, false)
         val layoutManager = LinearLayoutManager(activity)
+            //layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+            //val layoutManager = GridLayoutManager(activity, 2) //액티비티, 칼럼의 개수
         binding.recyclerView.layoutManager = layoutManager // 기본: vertical로 설정되어 있음
         binding.recyclerView.adapter = MyAdapter(datas)
-        //binding.recyclerView.addItemDecoration(MyDecoration(activity as Context)) // 데코레이션
+        binding.recyclerView.addItemDecoration(MyDecoration(activity as Context)) // 데코레이션
 
         return binding.root
     }
