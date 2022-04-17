24ca67dd621e9618da23cb68359e32a8b776beab
yxxyn20 <dbtndus0527@gmail.com>
2022-04-18 04:06:32 +0900

    ch13: Bundle 객체에 데이터 저장하기 - onSaveInstanceState(), savedInstanceState, let() 사용하기
    1) onSaveInstanceState 선언하여 액티비티가 전환되어 비활성화되었을 때 datas 값을 Bundle에 저장
    
    2) 액티비티가 비활성 -> 활성되었을 때 Bundle에 저장되었던 datas의 값 받기: savedInstanceState
    
    3) let()을 사용하여 객체(data에 대한)를 전달받기
    
    ** 액티비티가 활성화되었을 때 데이터를 받은 후 리사이클러 뷰를 호출해야한다. (안그러면 나타나지 않는 이슈가 발생)

--- pratice1/ch13/src/main/java/com/example/ch13/MainActivity.kt
+++ pratice1/ch13/src/main/java/com/example/ch13/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ch13
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.LayoutInflater
 import androidx.activity.result.ActivityResultLauncher
@@ -12,7 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ch13.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val datas : MutableList<String>? = null
+    var datas : MutableList<String>? = null
     lateinit var adapter : MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,22 +22,29 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // ActivityResultLauncher 사용하기: intent 사후처리
+        val requestLauncher : ActivityResultLauncher<Intent> = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            val resultData = it.data!!.getStringExtra("save_todo")?.let{
+                datas?.add(it) // datas에 데이터 추가
+                adapter.notifyDataSetChanged() // 리사이클러 뷰의 데이터 변경 명시
+            }
+        }
 
-        //리사이클러 뷰 생성
+        // 액티비티가 비활성 -> 활성되었을 때 Bundle에 저장되었던 datas의 값을 받는다.
+        datas = savedInstanceState?.let{
+            it.getStringArrayList("mydatas")?.toMutableList() // key 값을 통해 값을 얻어온다.
+        } ?: let{ // null일 때
+            mutableListOf<String>() // 리스트 생성 및 선언
+        }
+
+        //** 데이터를 가지고 온 후에 리사이클러 뷰 생성
         binding.mainRecyclerview.layoutManager = LinearLayoutManager(this)
         adapter = MyAdapter(datas)
         binding.mainRecyclerview.adapter = adapter
-//        binding.mainRecyclerview.addItemDecoration(
-//            DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
-//        )
-
-        // ActivityResultLauncher 사용하기: intent 사후처리
-        val requesteLauncher : ActivityResultLauncher<Intent> = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult())
-        {
-            val resultData = it.data?.getStringExtra("save_todo")
-            Log.d("test_intent", "ActivityResultLauncher: $resultData")
-        }
+        binding.mainRecyclerview.addItemDecoration(
+            DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
+        )
 
         // 인텐트 생성하기
         binding.fab.setOnClickListener {
@@ -45,17 +53,25 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("data2", " 저는 유수연입니다~!")
             //startActivity(intent)
             //startActivityForResult(intent, 10)
-            requesteLauncher.launch(intent) // ActivityResultLauncher로 호출하기
+            requestLauncher.launch(intent) // ActivityResultLauncher로 호출하기
         }
 
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    // datas의 값을 저장
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putStringArrayList("mydatas", ArrayList(datas)) // Bundle에 ArrayList 값 저장
+    }
+
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 10 && resultCode == RESULT_OK){
             val result = data?.getStringExtra("data1")
             Log.d("test_intent", "$result") // 이거 왜 안되지
         }
-    }
+    }*/
+
+
 }
