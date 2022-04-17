fd8e4a8445c9c0180e3f7959c1fefd16bf47c8ad
yxxyn20 <dbtndus0527@gmail.com>
2022-04-18 02:19:06 +0900

    ch13: 인텐트 적용하기 - RecyclerView와 EXtendedFloatingButton 적용하기

--- pratice1/ch13/src/main/java/com/example/ch13/MainActivity.kt
+++ pratice1/ch13/src/main/java/com/example/ch13/MainActivity.kt
@@ -1,32 +1,31 @@
-//package com.example.ch13
-//
-//import android.content.Intent
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import androidx.recyclerview.widget.DividerItemDecoration
-//import androidx.recyclerview.widget.LinearLayoutManager
-//import com.example.ch13.databinding.ActivityMainBinding
-//
-//class MainActivity : AppCompatActivity() {
-//    val datas : MutableList<String>? = null
-//    lateinit var adapter : MyAdapter
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        //setContentView(R.layout.activity_main)
-//        val binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        //리사이클러 뷰 생성
-//        binding.mainRecyclerview.layoutManager = LinearLayoutManager(this)
-//        adapter = MyAdapter(datas)
-//        binding.mainRecyclerview.adapter = adapter
-////        binding.mainRecyclerview.addItemDecoration(
-////            DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
-////        )
-//
-//        var intent = Intent(this, )
-//
-//    }
-//}
+package com.example.ch13
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ch13.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val datas : MutableList<String>? = null
+    lateinit var adapter : MyAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //리사이클러 뷰 생성
+        binding.mainRecyclerview.layoutManager = LinearLayoutManager(this)
+        adapter = MyAdapter(datas)
+        binding.mainRecyclerview.adapter = adapter
+//        binding.mainRecyclerview.addItemDecoration(
+//            DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
+//        )
+
+
+    }
+}
--- pratice1/ch13/src/main/java/com/example/ch13/MyAdapter.kt
+++ pratice1/ch13/src/main/java/com/example/ch13/MyAdapter.kt
@@ -1,25 +1,25 @@
-//package com.example.ch13
-//
-//import android.view.LayoutInflater
-//import android.view.ViewGroup
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.ch13.databinding.ItemRecyclerviewBinding
-//
-//class MyViewHolder(val binding: ItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root){
-//
-//}
-//
-//class MyAdapter(val datas: MutableList<String>?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
-//      = MyViewHolder(ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false ))
-//
-//
-//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-//        val binding = (holder as MyViewHolder).binding
-//        binding.itemTodo.text = datas!![position] //null 불허용
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return datas?.size ?: 0 // datas.size가 0일때 0 반환
-//    }
-//}
+package com.example.ch13
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ch13.databinding.ItemRecyclerviewBinding
+
+class MyViewHolder(val binding: ItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root){
+
+}
+
+class MyAdapter(val datas: MutableList<String>?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
+      = MyViewHolder(ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false ))
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+        binding.itemTodo.text = datas!![position] //null 불허용
+    }
+
+    override fun getItemCount(): Int {
+        return datas?.size ?: 0 // datas.size가 0일때 0 반환
+    }
+}
